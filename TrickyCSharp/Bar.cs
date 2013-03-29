da3c703c6d2c04663c5c0f0b3d49ac862326129d
Dmitri Nesteruk <dmitrinesteruk@gmail.com>
2013-03-29 15:23:54 +0400

    files for chuck no... ugh, i mean jon skeet

---
+++ TrickyCSharp/Bar.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace TrickyCSharp.Orcs
+{
+  public class Bar<T>
+  {
+    [ThreadStatic]
+    public ConcurrentDictionary<int, float> rates;
+
+    private static int range;
+
+    private string text;
+    private int value;
+
+    Bar(int n) : this("Chuck Norris")
+    {
+      value = n;
+    }
+
+    Bar(string s) : this(0)
+    {
+      text = s;
+    }
+
+    public void Print(string s)
+    {
+      s.Replace(" ", "_");
+      int value = s.Length;
+      if (s != null && s.Length != 0)
+        Console.WriteLine(s + " " + value);
+    }
+
+    public float Sum(float[,] array)
+    {
+      float sum = 0.0f;
+      for (int i = 0; i < array.GetLength(0); ++i)
+        for (int j = 0; j < array.GetLength(1); ++i)
+          sum += array[i, j];
+      return sum;
+    }
+
+    public void Product(float [] numbers)
+    {
+      // try converting to linq :)
+      float c = 1;
+      for (int i = 0; i < numbers.Length; ++i)
+        c *= numbers[i];
+      return c;
+    }
+  }
+}
---
+++ TrickyCSharp/Foo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using JetBrains.Annotations;
+
+namespace TrickyCSharp
+{
+  public partial class Foo : Form
+  {
+    public void CreateButtons(IList<string> labels)
+    {
+      foreach (String str in labels)
+      {
+        var btn = new Button();
+        btn.Click += (sender, args) => MessageBox.Show(str);
+      }
+    }
+
+    public void ProcessFiles([NotNull] int count)
+    {
+      var ofd = new OpenFileDialog();
+      ofd.ShowDialog();
+      var names = ofd.FileNames;
+
+      BeginInvoke(new Action<string[]>(Process), ofd.FileNames);
+    }
+
+    private void Process(string[] files)
+    {
+      if (files.Count() > 0)
+      {
+        // process each of the files
+        foreach (var file in files)
+          ProcessFile(file);
+      }
+    }
+
+    partial void ProcessFile(string filename);
+  };
+}
