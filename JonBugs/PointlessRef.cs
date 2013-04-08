b8ad261b2390c54c65bff517a398593a82635607
Jon Skeet <skeet@pobox.com>
2013-04-08 19:39:56 +0100

    PointlessRef - not sure why I didn't commit it before...

---
+++ JonBugs/PointlessRef.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace JonBugs
+{
+    class PointlessRef
+    {
+        public void Foo(ref StringBuilder builder)
+        {
+            builder.Append("Would work regardless of ref");
+        }
+
+        public void Swap(string first, string second)
+        {
+            string tmp = first;
+            first = second;
+            second = tmp;
+        }
+    }
+}
