31cb400cb5c054acece15f8648f470c57ca7b2a1
Jon Skeet <skeet@pobox.com>
2013-04-09 16:14:39 +0100

    Again, not sure why this didn't get committed before...

--- JonBugs/AwaitableInterface.cs
+++ JonBugs/AwaitableInterface.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using NUnit.Framework;
+using System;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework;
 
 namespace JonBugs
 {
